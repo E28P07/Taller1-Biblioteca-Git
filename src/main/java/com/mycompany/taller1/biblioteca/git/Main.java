441f0354b0680bd2c595113ad1ea389f24e58866
Esbel Plata <ejoseplata@unicesar.edu.co>
2026-08-16 18:54:16 -0500

    feat: crear clase Persona como Padre de Cliente

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller1.biblioteca.git;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Persona.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller1.biblioteca.git;
+
+
+public class Persona {
+    private String nombre;
+    private String cedula;
+
+    public Persona(String nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+    
+}
