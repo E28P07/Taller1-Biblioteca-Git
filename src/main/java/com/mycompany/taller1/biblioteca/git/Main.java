67d3c214cef8a73676f1054dec0ea07d41daa0cd
Esbel Plata <ejoseplata@unicesar.edu.co>
2026-08-17 09:56:11 -0500

    fix: ajustes en .gitignore y Main.java

--- src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.taller1.biblioteca.git;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Main {
 
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Aquí luego irá el menú
     }
 }
