5366e493ec0a12daa23a39cc7465aaf3c7e50205
Esbel Plata <ejoseplata@unicesar.edu.co>
2026-08-16 19:00:29 -0500

    feat: crear clase Cliente heredando de Persona

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Cliente.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller1.biblioteca.git;
+
+public class Cliente extends Persona {
+    private String direccion;
+    private String telefono;
+
+    public Cliente(String nombre, String cedula, String direccion, String telefono) {
+        super(nombre, cedula);
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+}
