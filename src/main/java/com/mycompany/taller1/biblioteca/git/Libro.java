de28186fac9d60d0ab7712b91c11fe601c3220aa
Esbel Plata <ejoseplata@unicesar.edu.co>
2026-08-17 10:45:19 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/taller1/biblioteca/git/Libro.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Libro.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.taller1.biblioteca.git;
 
-/**
- *
- * @author RCC Ing
- */
-public class Libro {
-    
+public class Libro extends Material {
+    private String editorial;
+    private int numeroPaginas;
+
+    public Libro(String titulo, String autor, String codigo, String editorial, int numeroPaginas) {
+        super(titulo, autor, codigo);
+        this.editorial = editorial;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
 }
